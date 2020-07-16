89621f43c9d9702c80cab938180db8038e97a0f3
Matthias Bandemer <git@bandemer.net>
2020-07-16 22:22:55 +0200

    Kalender entfernt

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,11 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class IndexController extends Controller
+class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="startseite")
@@ -33,35 +33,12 @@ class IndexController extends Controller
     }
 
     /**
-     * @Route("/kalender/")
-     */
-    public function kalender()
-    {
-        $session = new Session();
-
-        $notices = $session->getFlashBag()->get('notice', array());
-        $errors = $session->getFlashBag()->get('error', array());
-
-        $timestamp = filemtime('assets/podcastdepot.min.css');
-
-        return $this->render(
-            'kalender.html.twig',
-            array(
-                'csstime' => date('YmdHis', $timestamp),
-                'jahr' => date('Y'),
-                'notices' => $notices,
-                'errors' => $errors
-            ));
-    }
-
-    /**
      * @Route("/impressum/")
      */
     public function impressum()
     {
         return $this->render('impressum.html.twig');
     }
-
 
     /**
      * @Route("/datenschutz/")
